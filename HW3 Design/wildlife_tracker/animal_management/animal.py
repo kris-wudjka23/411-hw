b998f3b25af9f8f3a97e93d3cab513938b0bf348
Haotian Liu <krisliuu@bu.edu>
2024-10-07 15:31:47 -0400

    finish hw3

---
+++ HW3 Design/wildlife_tracker/animal_management/animal.py
@@ -0,0 +1,10 @@
+from typing import Any, Optional
+
+class Animal:
+    def __init__(self, animal_id: int, 
+                 species: str, 
+                 current_location: str,
+                 age: Optional[int] = None, 
+                 health_status: Optional[str] = None) -> None:
+        pass
+    pass
